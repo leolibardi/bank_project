7668f4a01bf480c5010b8573bfe21813a3aaee2e
Leonardo <leonardolibardi@outlook.com.br>
2025-02-19 14:40:05 -0300

    1.1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String username_admin = "admin";
-        String password_admin = "admin";
         String usernameRegistered = "Leonardo";
         String passwordRegistered = "123";
         String username;
